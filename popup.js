1c80d727da56b304e27f1b635b72d60febe1e924
Troels Mortensen <tromortensen@hotmail.com>
2025-12-25 22:55:02 +0100

    removed tempalte stuff

--- popup.js
+++
@@ -1,5 +0,0 @@
-// Popup script
-document.addEventListener('DOMContentLoaded', () => {
-  // Popup functionality can be added here in the future
-  console.log('GitHub Issues As Review Comments extension popup loaded');
-});
